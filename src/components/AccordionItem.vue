10aabf7bcd48ed1134282d67c2d11915aa6ba7b7
Nina Chrobok <n.chrobok@neusta.de>
2020-06-12 15:08:59 +0200

    [TASk] adds event-listener

--- src/components/AccordionItem.vue
+++ src/components/AccordionItem.vue
@@ -1,29 +1,35 @@
 <template>
-	<div class="c-accordion__item">
-		<div class="c-accordion__header">
-			<h2>{{ accordionTitle }}</h2>
-		</div>
-		<div class="c-accordion__content">{{ accordionContent }}</div>
-	</div>
+    <div class="c-accordion__item" :class="{ 'is-open': isOpen }">
+        <div @click="toggleAccordion" class="c-accordion__header">
+            <h2>{{ accordionTitle }}</h2>
+        </div>
+        <div class="c-accordion__content">{{ accordionContent }}</div>
+    </div>
 </template>
 
 <script>
-  export default {
-    name: 'Accordion',
-    props: {
-      accordionTitle: String,
-      accordionContent: {
-        type : String,
-        default: 'Default'
-      }
-    },
-    data() {
-      return {}
-    },
-    mounted() {
-    },
-    methods: {}
-  }
+    export default {
+        name: 'Accordion',
+        props: {
+            accordionTitle: String,
+            accordionContent: {
+                type: String,
+                default: 'Default'
+            }
+        },
+        data() {
+            return {
+                isOpen: false
+            }
+        },
+        mounted() {
+        },
+        methods: {
+            toggleAccordion() {
+                this.isOpen = !this.isOpen
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
