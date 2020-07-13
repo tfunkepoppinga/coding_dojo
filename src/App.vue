dacb16235e62b591e662d2d7830f2c37bd121558
Keith Barkhoff <k.barkhoff@neusta.de>
2020-07-13 13:27:13 +0200

    add initial autocomplete dojo

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,19 @@
 <template>
-	<div class="o-grid">
-		<div class="o-grid__item" :key="index" v-for="(card, index) in cards">
-			<card :headline="card.headline" :text="card.text" :image-src="card.imageSrc"></card>
-		</div>
-	</div>
+	<auto-complete />
 </template>
 
 <script>
-  import Card from './components/Card.vue'
+  import AutoComplete from './components/AutoComplete.vue'
 
   export default {
     name: 'App',
     components: {
-      Card
+      AutoComplete
     },
     props: {},
     data() {
       return {
-        cards: [
-          {
-            headline: "Headline 1",
-            text: "Lorem Ipsum",
-            imageSrc: "./static/assets/flex-1.jpg"
-          },
-          {
-            headline: "Headline 2",
-            text: "Lorem Ipsum dolor"
-          },
-          {
-            headline: "Headline 3",
-            text: "Lorem Ipsum dolor sit",
-            imageSrc: "./static/assets/flex-5.jpg"
-          }
-        ]
+
       }
     }
   }
