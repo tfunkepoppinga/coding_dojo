ed061557ef143377e9b926de1a201ad32ae6d692
Jesse Fresenborg <j.fresenborg@neusta.de>
2020-07-28 13:21:30 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
 	<div id="app">
-		<accordion/>
+		<modal :isOpen="isOpen" @close="isOpen = false"></modal>
+		<button @click="isOpen = true">Open Modal</button>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium aspernatur at corporis eligendi eos eum, illum maiores molestiae molestias necessitatibus non, provident ratione repellat repudiandae, temporibus velit vero voluptatum.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium aspernatur at corporis eligendi eos eum, illum maiores molestiae molestias necessitatibus non, provident ratione repellat repudiandae, temporibus velit vero voluptatum.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium aspernatur at corporis eligendi eos eum, illum maiores molestiae molestias necessitatibus non, provident ratione repellat repudiandae, temporibus velit vero voluptatum.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium aspernatur at corporis eligendi eos eum, illum maiores molestiae molestias necessitatibus non, provident ratione repellat repudiandae, temporibus velit vero voluptatum.
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium aspernatur at corporis eligendi eos eum, illum maiores molestiae molestias necessitatibus non, provident ratione repellat repudiandae, temporibus velit vero voluptatum.
+
 	</div>
 </template>
 
 <script>
-  import Accordion from './components/Accordion.vue'
-
+import Modal from "./components/Modal"
   export default {
     name: 'App',
     components: {
-      Accordion
+      Modal
+    },
+    data() {
+      return {
+        isOpen: false,
+      }
     }
   }
 </script>
 <style lang="scss">
 	@import 'scss/03-generic/generic.normalize.scss';
+	body {
+		height: 100vh;
+		margin: 0 auto;
+	}
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="c-modal" :class="{'is-open' : isOpen}">
+		<div class="c-modal__backdrop"></div>
+		<div class="c-modal__content">
+			<div class="c-modal__close" @click="triggerButton">X</div>
+			<div class="c-modal__header"><h3>{{ modalHeadline }}</h3></div>
+			<div class="c-modal__body">
+				<p>{{ modalText }}</p>
+			</div>
+			<div class="c-modal__footer">
+				{{ modalFooterText }}
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+  export default {
+    name: "modal",
+    props: {
+      modalHeadline: {
+        type: String,
+        default: "Headline"
+      },
+      modalText: {
+        type: String,
+        default: "Modal Text"
+      },
+      modalFooterText: {
+        type: String,
+        default: "Footer"
+      },
+      isOpen: {
+        type: Boolean,
+        default: false
+      }
+
+
+    },
+    data() {
+      return {
+      }
+    },
+    methods: {
+      triggerButton() {
+        this.$emit('close')
+      }
+    }
+  }
+</script>
+
+<style  lang="scss" scoped>
+	@import "../scss/06-components/_components.modal.scss";
+</style>
