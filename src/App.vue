4a938f2788d5293daf88b22d49a9b6b972ebe0a6
Jesse Fresenborg <j.fresenborg@neusta.de>
2020-08-26 14:53:25 +0200

    [TASK] api multiselect

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
-
+        <multiselect :options="options" v-model="value"  label="title" track-by="id" placeholder="Bitte auswählen.." :taggable="true" :customLabel="firstWord">
+<!--            <template slot="option" slot-scope="{ option }"><strong class="test">{{ option.title }}</strong></template>-->
+        </multiselect>
     </div>
 </template>
 
 <script>
+
 export default {
     name: 'App',
     components: {
@@ -12,10 +15,37 @@ export default {
     props: {},
     data() {
         return {
+            value: null,
+            options: [{
+            }],
+
         }
+    },
+    methods: {
+        async getInfoFromApi() {
+            const API = 'https://jsonplaceholder.typicode.com/todos';
+            fetch(API).then( (result) => result.json()).then( (options) => this.options = options)
+        },
+        firstWord({ title }) {
+            if (!title) {
+                return ''
+            } else {
+                const cutFrom = 0;
+                const cutHere = title.indexOf(' ');
+                return title.substring(cutFrom, cutHere);
+            }
+        }
+    },
+    computed: {
+
+    },
+    mounted() {
+        this.getInfoFromApi();
     }
 }
 </script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 
 <style lang="scss">
 @import './scss/main';
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Multiselect from "vue-multiselect";
 
 Vue.config.productionTip = false
+// register globally
+Vue.component('multiselect', Multiselect)
 
 new Vue({
   render: h => h(App),
