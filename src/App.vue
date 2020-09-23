7fdc589d3af3b695fad3c86ea83d44fcd3527f9f
Jesse Fresenborg <j.fresenborg@neusta.de>
2020-09-23 12:51:12 +0200

    [JESSE]

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <template>
-   <div>
-
-   </div>
+   <v-app>
+       <v-navigation-drawer permanent>
+           <v-list-item link to="/Index">
+               <v-list-item-content>
+                   <v-list-item-title class="title">
+                       Index
+                   </v-list-item-title>
+                   <v-list-item-subtitle>
+                       Stuff
+                   </v-list-item-subtitle>
+               </v-list-item-content>
+           </v-list-item>
+           <v-list-item link to="/About">
+               <v-list-item-content>
+                   <v-list-item-title class="title">
+                       About
+                   </v-list-item-title>
+                   <v-list-item-subtitle>
+                       Other Stuff
+                   </v-list-item-subtitle>
+               </v-list-item-content>
+           </v-list-item>
+       </v-navigation-drawer>
+   </v-app>
 </template>
 
 <script>
--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,4 +1,22 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+import About from '../components/About'
+import Index from '../components/Index'
+
 Vue.use(VueRouter);
+
+const Routes = [
+    {
+        path: '/testo',
+        component: About
+    },
+    {
+        path: '/Index',
+        component: Index
+    }
+]
+
+export default new VueRouter({
+    Routes
+});
